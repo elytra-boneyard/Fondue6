bfc075f95be648683b203345c40bb4679a0fee6d
Aesen Vismea <aesen@unascribed.com>
2017-03-23 00:44:07 -0400

    Automatically delete jars with no matching .url.txt

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
+find ./src/ -name '*.jar' | while read file; do
+	if [ ! -e "${file}.url.txt" ]; then
+		echo Deleting dangling file `basename $file`...
+		rm $file
+	fi
+done
 find ./ -name '*.url.txt' | while read file; do
 	basename=${file%.url.txt}
 	if [ ! -e "$basename" ]; then
@@ -9,19 +15,17 @@ find ./ -name '*.url.txt' | while read file; do
 		echo Skipping download of existing file `basename $basename`
 	fi
 done
-if [ -z "$1" ]; then
-	cd mod
-	if [ ! -d ".gradle" ]; then
-		echo Preparing to build the Fondue mod...
-		# setupDevWorkspace and setupCiWorkspace do not include
-		# generic type information, and are pretty broken as a
-		# result. Don't change this.
-		./gradlew setupDecompWorkspace
-	fi
-	echo Building the Fondue mod...
-	./gradlew clean build
-	rm -f ../src/mods/1.11.2/Fondue-*.jar
-	rm build/libs/*-slim.jar
-	rm build/libs/*-sources.jar
-	cp build/libs/* ../src/mods/1.11.2/
+cd mod
+if [ ! -d ".gradle" ]; then
+	echo Preparing to build the Fondue mod...
+	# setupDevWorkspace and setupCiWorkspace do not include
+	# generic type information, and are pretty broken as a
+	# result. Don't change this.
+	./gradlew setupDecompWorkspace
 fi
+echo Building the Fondue mod...
+./gradlew clean build
+rm -f ../src/mods/1.11.2/Fondue-*.jar
+rm build/libs/*-slim.jar
+rm build/libs/*-sources.jar
+cp build/libs/* ../src/mods/1.11.2/
